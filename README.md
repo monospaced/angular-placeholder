7a8d09f1ba2c9c1a25aedead9e4df1174dd36d64
Scott Boyle <scott@monospaced.com>
2013-09-09 12:32:44 +0100

    first version

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 angular-placeholder
 ===================
 
-AngularJS placeholder polyfill
+AngularJS placeholder polyfill, without jQuery dependency
+
+
+[See it in action](http://monospaced.github.io/angular-placeholder).
+
+Usage
+-----
+
+as attribute
+
+    <input type="text" msd-placeholder="Placeholder text">
+
+Install
+-------
+
+    bower install monospaced/angular-placeholder
+
+Include the `placeholder.js` script provided by this component in your app, and add `monospaced.placeholder` to your app’s dependencies.
+
+Support
+-------
+
+No jQuery required, tested in these [core browsers](http://monospaced.github.io/obs).
+
+Demo
+----------------
+
+[monospaced.github.io/angular-placeholder](http://monospaced.github.io/angular-placeholder)
---
+++ placeholder.js
@@ -0,0 +1,83 @@
+/*
+ * angular-placeholder v0.0.1
+ * (c) 2013 Monospaced http://monospaced.com
+ * License: MIT
+ */
+
+angular.module('monospaced.placeholder', [])
+  .directive('msdPlaceholder', ['$timeout', '$window', function($timeout, $window){
+    'use strict';
+
+    var placeholderColor = '#888';
+
+    return {
+      require: 'ngModel',
+      link: function(scope, element, attrs, ngModel){
+        var doc = $window.document,
+            domElement = element[0];
+
+        if (domElement.nodeName !== 'TEXTAREA' &&
+            domElement.nodeName !== 'INPUT' ||
+            attrs.type === 'password') {
+          return;
+        }
+
+        var isInputSupported = 'placeholder' in doc.createElement('input'),
+            isTextareaSupported = 'placeholder' in doc.createElement('textarea');
+
+        if (isInputSupported && isTextareaSupported) {
+          $timeout(function(){
+            element.attr('placeholder', attrs.msdPlaceholder);
+          });
+          return;
+        }
+
+        function init(value){
+          if (doc.activeElement === domElement) {
+            return;
+          }
+
+          if (value === '' || value === attrs.msdPlaceholder) {
+            domElement.value = attrs.msdPlaceholder;
+            domElement.style.color = placeholderColor;
+          }
+        }
+
+        function focus(){
+          if (domElement.value == attrs.msdPlaceholder) {
+            domElement.value = '';
+            domElement.style.color = '';
+          }
+        }
+
+        function blur(){
+          if (domElement.value === '') {
+            domElement.value = attrs.msdPlaceholder;
+            domElement.style.color = placeholderColor;
+          }
+        }
+
+        $timeout(function(){
+          init(domElement.value);
+        });
+
+        scope.$watch(function(){
+          return ngModel.$modelValue;
+        }, function(newValue){
+          init(newValue);
+        });
+
+        element.bind('focus', focus);
+        element.bind('blur', blur);
+
+        /*
+         * destroy
+         */
+
+        scope.$on('$destroy', function(){
+          element.unbind('focus', focus);
+          element.unbind('blur', blur);
+        });
+      }
+    };
+  }]);
